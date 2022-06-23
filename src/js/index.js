853234fe76c4746bbd98911e51f2f1c479c2d2fc
BatynIvan <batiniwan1@gmail.com>
2022-06-23 09:44:46 +0300

    Add function (calculation of the quadratic equation)

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+/* const age = prompt('Скільки Вам років?');
+let message = 'Доступ заборонено';
+
+if (age>=18) {
+  message = 'Привіт';
+}
+
+alert(message);
+ */
--- src/js/node.js
+++
@@ -1,8 +0,0 @@
-let age = prompt('Скільки Вам років?');
-
-if (age >= 18) {
-  message = 'Привіт!';
-} else if (age < 18) {
-  message = 'Будь ласка покиньте сайт.';
-}
-alert(message);
